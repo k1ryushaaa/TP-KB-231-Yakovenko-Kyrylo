9a9327af8211cb875a72035cdfbce93d9e8825a9
Kyrylo <pufikgaming@gmail.com>
2024-10-04 13:41:38 +0300

    changes for topic 01

---
+++ topic_01/task1.py
@@ -0,0 +1,8 @@
+# Вхідний рядок
+input_string = "abcdefg123"
+
+# Перевертаємо рядок за допомогою зрізів
+reversed_string = input_string[::-1]
+
+# Виводимо результат
+print(reversed_string)
---
+++ topic_01/task2.py
@@ -0,0 +1,18 @@
+# Тестовий рядок
+test_str = " hello world! "
+print(f"Оригінал: '{test_str}'")
+
+# 1. strip(видаляє пробіли або інші вказані символи з початку і кінця рядка)
+print(f"strip: '{test_str.strip()}'")
+
+# 2. capitalize(робить першу літеру рядка великою, а всі інші — малими.)
+print(f"capitalize: '{test_str.capitalize()}'")
+
+# 3. title(робить кожне слово в рядку з великої літери.)
+print(f"title: '{test_str.title()}'")
+
+# 4. upper(переводить всі символи рядка в верхній регістр.)
+print(f"upper: '{test_str.upper()}'")
+
+# 5. lower(переводить всі символи рядка в нижній регістр.)
+print(f"lower: '{test_str.lower()}'")
